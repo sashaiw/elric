6b5a19fcf97d2f441b6ba0c0c2193cc72ca5830f
Sasha Wilkinson <sashaiw@gmail.com>
2017-10-23 13:39:07 -0400

    Added LPF, major cleanup.

--- elric.ino
+++ elric.ino
@@ -1,52 +1,71 @@
 #include <Servo.h>
+#include <Filters.h>
 
-const int SIGMOID_GAIN = 2;
-const int MIN_INPUT = 10;
-const int MAX_INPUT = 707; 
-const int SMOOTH_LENGTH = 10;
+// Tune-able parameters
+const int POLL_RATE        = 10;  // Poll rate in ms
+const int SIGMOID_GAIN     = 2;   // Gain for sigmoid function (e^-x)
+const int MIN_INPUT        = 40;  // Minimum input value (0-1024
+const int MAX_INPUT        = 200; // Maximum input value (0-1024)
+const int FILTER_FREQUENCY = 100; // Frequency in HZ of LPF
+const int SMOOTH_LENGTH    = 5;   // Length of average 
 
-const int MOTOR_PIN = 10;
-const int SENSOR_PIN = 0;
+// Pins
+const int MOTOR_PIN = 10; // Servo
+const int SENSOR_PIN = 0; // Myoelectric sensor
 
-int input_readings[SMOOTH_LENGTH];
-int input_read_index = 0;
-float input_total = 0;
-float input_average = 0;
 
 Servo servo;
+FilterOnePole lpf(LOWPASS, FILTER_FREQUENCY);
 
 void setup() {
   Serial.begin(115200);
   servo.attach(MOTOR_PIN);
-
-  for (int i = 0; i < SMOOTH_LENGTH; i++) {
-    input_readings[i] = 0;
-  }
 }
 
 void loop() {
-  int input = analogRead(SENSOR_PIN);
-  Serial.print("Raw: ");
-  Serial.println(input);
+  int input_value;
+  float output_value;
+  
+  // Get input value with LPF
+  input_value = analogRead(SENSOR_PIN);
 
-  // Smooth that shit
+  // Apply bullshit scaling math
+  output_value = scale_to_range(output_value, MIN_INPUT, MAX_INPUT, 0, 1);
+
+  // Apply LPF
+  output_value = lpf.input(input_value);
+  
+  // Average
+  output_value = smooth_that_shit(SMOOTH_LENGTH, input_value);
+  
+  // Apply sigmoid curve
+  output_value = sigmoid(output_value, SIGMOID_GAIN);
+
+  // Debug, plot two graphs
+  Serial.print(input_value);
+  Serial.print(",");
+  Serial.println(output_value * 1024);
+
+  // Write to servo
+  servo.write(output_value * 180);
+  delay(POLL_RATE);
+}
+
+float smooth_that_shit(int smooth_length, float input_value) {
+  static int input_readings[SMOOTH_LENGTH];
+  static int input_read_index = 0;
+  static float input_total = 0;
+  static float input_average = 0;
+
   input_total -= input_readings[input_read_index];
-  input_readings[input_read_index] = input;
+  input_readings[input_read_index] = input_value;
   input_total += input_readings[input_read_index];
   input_read_index++;
-  if (input_read_index >= SMOOTH_LENGTH) { input_read_index = 0; }
+  if (input_read_index >= SMOOTH_LENGTH) {
+    input_read_index = 0;
+  }
 
-  float motorval = input_total / SMOOTH_LENGTH;
-  
-  // BULLSHIT MATH
-  // Scale between 0 and 1
-  motorval = scale_to_range(motorval, MIN_INPUT, MAX_INPUT, 0, 1);
-  // sigmoid curve
-  motorval = sigmoid(motorval, SIGMOID_GAIN);
-
-  Serial.println(motorval * 1024);
-  servo.write(motorval * 180);
-  delay(50);
+  return input_total / SMOOTH_LENGTH;
 }
 
 float scale_to_range(float input, int old_min, int old_max, int new_min, int new_max) {
@@ -54,6 +73,6 @@ float scale_to_range(float input, int old_min, int old_max, int new_min, int new
 }
 
 float sigmoid(float input, float gain) {
-  return 1 / (1 + pow(2.71828, (gain - 2 * gain) * (input - 0.5) ));
+  return 1 / (1 + pow(2.71828, (-gain) * (input - 0.5) ));
 }
 
