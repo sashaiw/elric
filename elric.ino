b50d3db026a24051b6d581930818a721e930d9e5
Sasha Wilkinson <sashaiw@gmail.com>
2017-11-02 20:10:05 -0400

    mom how do i math??!?!???

--- elric.ino
+++ elric.ino
@@ -3,13 +3,13 @@
 
 // Tune-able parameters
 const int POLL_RATE          = 10;   // Poll rate in ms
-const int SIGMOID_GAIN       = 1;   // Gain for sigmoid function (e^-x)
-const int DEFAULT_MIN_INPUT  = 200;  // Minimum input value (0-1024
-const int DEFAULT_MAX_INPUT  = 250;  // Maximum input value (0-1024)
-const int CALIBRATE_LENGTH   = 50;   // Number of values to collect for calibration
-const int CALIBRATE_TIME     = 5000; // Amount of time 
-const float FILTER_FREQUENCY = 1;   // Frequency in HZ of LPF
+const int SIGMOID_GAIN       = 1;    // Gain for sigmoid function (e^-x)
+const int DEFAULT_MIN_INPUT  = 50;  // Minimum input value (0-1024
+const int DEFAULT_MAX_INPUT  = 120;  // Maximum input value (0-1024)
 const int RANGE_DELTA        = 25;   // Delta for calibrated range
+const int CALIBRATE_LENGTH   = 50;   // Number of values to collect for calibration
+const int CALIBRATE_TIME     = 5000; // Duration to collect calibration data on
+const float FILTER_FREQUENCY = 1;    // Frequency in HZ of LPF
 
 // Pins
 const int MOTOR_PIN = 10; // Servo
@@ -36,7 +36,7 @@ void loop() {
 
   // Calibration buttons
   if (digitalRead(MIN_BUTTON) == LOW) {
-    min_input = calibrate_values() + RANGE_DELTA; 
+    min_input = calibrate_values() + RANGE_DELTA;
     Serial.print("Min input calibrated to ");
     Serial.println(min_input);
   }
@@ -45,48 +45,52 @@ void loop() {
     Serial.print("Max input calibrated to ");
     Serial.println(max_input);
   }
-  
+
   // Apply LPF
   lpf.input(input_value);
   output_value = lpf.output();
+  //output_value = input_value;
 
   // Scale to range
   output_value = scale_to_range(output_value, min_input, max_input, 0, 1);
 
-  // Apply sigmoid curve
-  output_value = sigmoid(output_value, SIGMOID_GAIN);
-
   // Debug, plot two graphs
-  Serial.print(scale_to_range(input_value, 0, 1024, 0, 1024));
+  Serial.print(scale_to_range(input_value, min_input, max_input, min_input, max_input));
   Serial.print(",");
-  Serial.print(output_value * 1024);
+  Serial.print(scale_to_range(output_value, 0, 1, min_input, max_input));
   Serial.print(",");
   Serial.print(min_input);
   Serial.print(",");
   Serial.println(max_input);
-  
+
   // Write to servo
   servo.write(scale_to_range(output_value, 0, 1, 0, 180));
   delay(POLL_RATE);
 }
 
 float scale_to_range(float input_value, float old_min, float old_max, float new_min, float new_max) {
-  return (input_value - old_min) * (new_max - new_min) / (old_max - old_min) + new_min;
+  return clamp((input_value - old_min) * (new_max - new_min) / (old_max - old_min) + new_min, new_min, new_max);
 }
 
-float sigmoid(float input, float gain) {
-  return 1 / (1 + pow(2.71828, (gain - 2 * gain) * (input - 0.5) ));
+float clamp(float x, float min_value, float max_value) {
+  if (x < min_value) {
+    x = min_value;
+  }
+  if (x > max_value) {
+    x = max_value;
+  }
+  return x;
 }
 
 int calibrate_values() {
   Serial.println("CALIBRATING");
   int avg = 0;
   int readings[CALIBRATE_LENGTH];
-  for(int i = 0; i < CALIBRATE_LENGTH; i++) {
-    readings[i]=analogRead(SENSOR_PIN);
-    delay(CALIBRATE_TIME/CALIBRATE_LENGTH);
+  for (int i = 0; i < CALIBRATE_LENGTH; i++) {
+    readings[i] = analogRead(SENSOR_PIN);
+    delay(CALIBRATE_TIME / CALIBRATE_LENGTH);
   }
-  for(int i = 0; i < CALIBRATE_LENGTH; i++) {
+  for (int i = 0; i < CALIBRATE_LENGTH; i++) {
     avg += readings[i];
   }
   avg /= CALIBRATE_LENGTH;
